d2d4903a312ca6d0ffae0237308c4467a9a0e935
Armando Kwiek <armando.kwiek@ericsson.com>
2021-06-08 22:34:04 +0200

    login screen UI

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
         <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,70 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <View>
-            <Text>LoginScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Login Screen</Text>
+            <TextInput style={styles.textInput}
+                onChangeText={email => setEmail(email)}
+                email={email}
+                placeholder='  email@gmail.com'
+                placeholderTextColor='gray'
+            />
+            <TextInput style={styles.textInput}
+                onChangeText={email => setEmail(email)}
+                email={email}
+                placeholder='  password'
+                placeholderTextColor='gray'
+            />
+            <TouchableOpacity>
+                <View style={[styles.button, {justifyContent:'center', alignItems:'center'}]}>
+                    <Text style={styles.buttonText}> Login </Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <View style={[styles.button, {justifyContent:'center', alignItems:'center'}]}>
+                    <Text style={styles.buttonText}> Sign Up! </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'white',
+        fontSize: 40,
+        marginBottom: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 25
+    },
+    textInput: {
+        height: 50,
+        width: 300,
+        borderColor: 'gray',
+        borderWidth: 1,
+        color: 'white',
+        margin: 10,
+        fontSize: 20
+    },
+    button: {
+        width: 300,
+        height: 50,
+        backgroundColor: 'blue',
+        marginTop: 20
+    }
+})
 
 export default LoginScreen
